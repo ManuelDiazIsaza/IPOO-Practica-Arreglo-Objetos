a313ec1e3b84c200b68f420c81701488473be8d4
ManuelDiazIsaza <37357573+ManuelDiazIsaza@users.noreply.github.com>
2018-03-15 23:11:43 -0500

    Punto 2 3
    Punt 2 3 Agregado

---
+++ cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/Estudiante.cpp" "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/Estudiante.cpp.obj"
+  "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/Grupo.cpp" "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/Grupo.cpp.obj"
+  "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/main.cpp" "G:/Google Drive/Univalle/Semestre 2/IPOO/Ejemplo de Clase 2/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
